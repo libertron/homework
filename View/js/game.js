e652444fb6d8a1dcada618becce034fc21e4d855
libertron <marcusserginio96@gmail.com>
2018-12-19 00:26:14 -0500

    real optimization have been done at this part

--- View/js/game.js
+++ View/js/game.js
@@ -1,28 +1,28 @@
 function Game(data){
 	this.gameDataExist=typeof(data)!=='undefined';
-	this.ballIDID=(this.gameDataExist && typeof(data.ballID)=='string')?data.ballID:null;
-	this.scoreID=(this.gameDataExist && typeof(data.ballID)=='string')?data.score:null;
+	this.ballID=(this.gameDataExist && typeof(data.ball)=='string')?data.ball:null;
+	this.scoreID=(this.gameDataExist && typeof(data.scoreScreen)=='string')?data.scoreScreen:null;
 	this.score=0
-	this.square=(this.gameDataExist && typeof(data.square)=='string')?data.square:null;
-	this.timer=0;
+	this.square=(this.gameDataExist && typeof(data.squarePan)=='string')?data.squarePan:null;
+	this.timer=(this.gameDataExist && typeof(data.timer)=='string')?data.timer:null;
 	this.randomXY=function(){
 		return {};
 	};
 	this.init=function(){
 		if(this.gameDataExist){
-			if(typeof(data.score)=='string'){
-				document.querySelector('#'+data.score).value=0;
+			if(this.scoreID!==null){
+				document.querySelector('#'+this.scoreID).value=0;
 			}
-			if(typeof(data.timer)=='string'){
-				document.querySelector('#'+data.timer).value=0+' ms';
+			if(this.timer!==null){
+				document.querySelector('#'+this.timer).value=0+' ms';
 			}
 		}
 	};
 	this.incrementScore=function(){
 		this.score++;
 
-		if(this.gameDataExist && typeof(data.score)=='string'){
-			document.querySelector('#'+data.score).value=this.score;
+		if(this.gameDataExist && this.scoreID!==null){
+			document.querySelector('#'+this.scoreID).value=this.score;
 		}
 	};
 	this.start=function(){
@@ -30,7 +30,7 @@ function Game(data){
 	};
 	this.hideBall=function(){
 		setTimeout(function(){
-			
+			document.querySelector('#'+ballID).style.display='none';
 
 		},1000,ballID=this.ballID);
 	};
